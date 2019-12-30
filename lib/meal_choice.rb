a6fbcd5a3402b2685e725832a5d209401622aeb4
Beza Sirak <sirak.bezawit@gmail.com>
2019-12-30 06:05:15 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,6 +2,6 @@
 # For output purposes, use "puts" instead of "print" or "p"
 
 def meal_choice( protein = "meat", veg1, veg2)
-  puts "What a nutritius meal!"
-  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  p "What a nutritius meal!"
+  p "A plate of #{protein} with #{veg1} and #{veg2}."
 end
